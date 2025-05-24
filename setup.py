d4aaed7075dbb5191e039d1a6d8553a5c5104a94
Ayush <ayushbhujade2005@gmail.com>
2025-05-24 11:00:03 +0530

    project structure is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Ayush Bhujade',
+    author_email='ayushbhujade2005@gmail.com',
+    packages=find_packages()
+)
